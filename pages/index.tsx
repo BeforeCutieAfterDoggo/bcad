f1a0c0de1f8c4375b3adb87dff16802782fd9e26
Lucy Qiu <lucy1049684100@gmail.com>
2023-01-28 16:58:34 -0800

    s

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,24 +20,27 @@ export default function Home(): any {
         <title>BCAD</title>
 
       </Head>
-      <main className="min-h-screen pb-14 flex justify-center">
+      <main className="min-h-screen pb-14 flex justify-center relative">
         <img className="top-0 left-0 border border-white border-2 w-screen h-screen" src="/bgs/bg0.png" />
+        {/* <p className='absolute text-white font-mono my-36'>Before Cutie, After Doggo</p> */}
         <img className=" absolute m-auto my-48 border border-white border-2 justify-center w-1/4 h-auto" src="/images/1.png" />
+        {/* <p className='text-center absolute text-white font-mono bottom-40 inset-x-0'>Beautifully Crafted, Aptly Developed</p> */}
       </main>
+
       <Collapse style={{ border: "2px solid white", borderRadius: 0 }} defaultActiveKey={['1']}>
-        <StyledPanel  style={{cursor:'pointer'}}header={<span className="text-3xl font-mono" style={{cursor:'pointer',color: "white", display: "flex", justifyContent: "center", alignItems: "center" }}>DROP #000001</span>} key="1">
+        <StyledPanel showArrow={false} style={{ cursor: 'pointer' }} header={<span className="text-3xl font-mono" style={{ cursor: 'pointer', color: "white", display: "flex", justifyContent: "center", alignItems: "center" }}>DROP #000001</span>} key="1">
 
           <div>
             <p className=' font-mono mb-2'>🍄 The Trip Book</p>
             <img src="/drops/drop1.png" />
           </div>
         </StyledPanel>
-        <StyledPanel header={<span className="text-3xl font-mono" style={{ color: "white",  display: "flex", justifyContent: "center", alignItems: "center" }}>DROP #000002</span>} key="2">
+        <StyledPanel showArrow={false} header={<span className="text-3xl font-mono" style={{ color: "white", display: "flex", justifyContent: "center", alignItems: "center" }}>DROP #000002</span>} key="2">
           <div >
             <p>2</p>
           </div>
         </StyledPanel>
-        <StyledPanel header={<span className="text-3xl font-mono" style={{ color: "white",  display: "flex", justifyContent: "center", alignItems: "center" }}>DROP #000003</span>} key="3">
+        <StyledPanel showArrow={false} header={<span className="text-3xl font-mono" style={{ color: "white", display: "flex", justifyContent: "center", alignItems: "center" }}>DROP #000003</span>} key="3">
           <p>3</p>
         </StyledPanel>
       </Collapse>
