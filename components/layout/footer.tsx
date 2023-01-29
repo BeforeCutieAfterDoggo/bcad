125190b91ccb7e7e6ec2e95470bcc22a2f8a05a9
Lucy Qiu <lucy1049684100@gmail.com>
2023-01-28 17:01:46 -0800

    s

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -2,7 +2,7 @@
 import Link from 'next/link';
 export default function Footer(props: any) {
 
-    return <footer className="bg-[#1E1E1E] p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 lg:flex lg:items-center lg:justify-between lg:p-6 ">
+    return <footer className="bg-[#1E1E1E] p-4 rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 lg:flex lg:items-center lg:justify-between lg:p-6 ">
         <div className='ml-4'>
             <p className='text-white'>BEFORE CUTIE AFTER DOGGO, BEAUTIFULLY CRAFTED APTLY DEVELOPED.</p>
         </div>
