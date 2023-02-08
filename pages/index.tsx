9643bf2f4dc32277004b1b61a9aea721cdc7734e
Lucy Qiu <lucy1049684100@gmail.com>
2023-02-08 10:58:14 -0800

    b

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,17 +22,16 @@ export default function Home(): any {
 
       </Head>
       <main className="min-h-screen pb-14 flex justify-center relative">
-        <img className="top-0 left-0 border border-white border-2 w-screen h-screen" src="/bgs/bg0.png" />
+      <img className="top-0 left-0 border border-white border-2 w-screen h-screen object-cover" src="/bgs/bg0.png" />
         {/* <p className='absolute text-white font-mono my-36'>Before Cutie, After Doggo</p> */}
-        <img className=" absolute m-auto my-48 border border-white border-2 justify-center w-1/4 h-auto" src="/images/1.png" />
+        <img className=" absolute m-auto my-64 border border-white border-2 justify-center w-1/2 md:w-1/3 md:my-40 lg:w-1/4 lg:my-36 h-auto xl:w-1/4 xl:my-32 2xl:w-1/4 2xl:my-24 " src="/images/1.png" />
         {/* <p className='text-center absolute text-white font-mono bottom-40 inset-x-0'>Beautifully Crafted, Aptly Developed</p> */}
       </main>
 
       <Collapse style={{ border: "2px solid white", borderRadius: 0 }} defaultActiveKey={['1']}>
-        <StyledPanel showArrow={false} style={{ cursor: 'pointer' }} header={<span className="text-3xl font-mono" style={{ cursor: 'pointer', color: "white", display: "flex", justifyContent: "center", alignItems: "center" }}>DROP #000001</span>} key="1">
+        <StyledPanel showArrow={false} style={{ cursor: 'pointer' }} header={<span className="text-3xl font-mono" style={{ cursor: 'pointer', color: "white", display: "flex", justifyContent: "center", alignItems: "center" }}>DROP #000001 🍄 The Trip Book</span>} key="1">
 
-          <div>
-            <p className=' font-mono mb-2'>🍄 The Trip Book</p>
+          <div className='xl:bg-[#E4E4E2] xl:text-black'>
             <Link href='/project/1'>
             <img className='mx-auto' src="/drops/drop1.png" />
             </Link>
--- pages/project/1.tsx
+++ pages/project/1.tsx
@@ -17,7 +17,7 @@ export default function ProductPage() {
                     </h1>
 
                 </div>
-                <div className="mx-auto mt-6 text-gray-500 divide divide-y-2 divide-black lg:bg-gray-200">
+                <div className="mx-auto mt-6 text-gray-500 divide divide-y-2 divide-black xl:bg-[#E4E4E2]">
                     <img className="mx-auto" src="/drops/drop1/1.png"></img>
                     <div className="md:flex justify-center">
                         <div className="w-full md:w-1/2 lg:w-1/2 xl:w-1/2 ">
