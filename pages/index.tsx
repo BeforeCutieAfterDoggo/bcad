e1e6dcaa09b8f3cbd5f9b1d3b010309f70944dfd
Lucy Qiu <lucy1049684100@gmail.com>
2023-02-09 10:49:14 -0800

    s

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,29 +22,34 @@ export default function Home(): any {
 
       </Head>
       <main className="min-h-screen pb-14 flex justify-center relative">
-      <img className="top-0 left-0 border border-white border-2 w-screen h-screen object-cover" src="/bgs/bg0.png" />
+        <img className="top-0 left-0 border border-white border-2 w-screen h-screen object-cover" src="/bgs/bg0.png" />
         {/* <p className='absolute text-white font-mono my-36'>Before Cutie, After Doggo</p> */}
         <img className=" absolute m-auto my-64 border border-white border-2 justify-center w-1/2 md:w-1/3 md:my-40 lg:w-1/4 lg:my-36 h-auto xl:w-1/4 xl:my-32 2xl:w-1/4 2xl:my-24 " src="/images/1.png" />
         {/* <p className='text-center absolute text-white font-mono bottom-40 inset-x-0'>Beautifully Crafted, Aptly Developed</p> */}
       </main>
 
-      <Collapse style={{ border: "2px solid white", borderRadius: 0 }} defaultActiveKey={['1']}>
+      <Collapse style={{ border: "2px solid white", borderRadius: 0 }} defaultActiveKey={['2']}>
         <StyledPanel showArrow={false} style={{ cursor: 'pointer' }} header={<span className="text-3xl font-mono" style={{ cursor: 'pointer', color: "white", display: "flex", justifyContent: "center", alignItems: "center" }}>DROP #000001 🍄 The Trip Book</span>} key="1">
 
           <div className='xl:bg-[#E4E4E2] xl:text-black'>
             <Link href='/project/1'>
-            <img className='mx-auto' src="/drops/drop1.png" />
+              <img className='mx-auto' src="/drops/drop1.png" />
             </Link>
           </div>
         </StyledPanel>
-        <StyledPanel showArrow={false} header={<span className="text-3xl font-mono" style={{ color: "white", display: "flex", justifyContent: "center", alignItems: "center" }}>DROP #000002</span>} key="2">
-          <div >
-            <p>Coming Soon</p>
+        <StyledPanel showArrow={false} header={<span className="text-3xl font-mono" style={{ color: "white", display: "flex", justifyContent: "center", alignItems: "center" }}>DROP #000002 🍝 Meet Spaghettify</span>} key="2">
+          <div className='relative bg-white' >
+            <img className="absolute inset-0 h-96 w-screen object-cover " src="/drops/drop2/grad2.png" alt="" />
+            <Link href='/project/2'>
+
+            <img style={{cursor:"pointer"}}className="relative mx-auto" src="/drops/drop2/drop2.png"></img>
+          </Link>
           </div>
+
         </StyledPanel>
-        <StyledPanel showArrow={false} header={<span className="text-3xl font-mono" style={{ color: "white", display: "flex", justifyContent: "center", alignItems: "center" }}>DROP #000003</span>} key="3">
+        {/* <StyledPanel showArrow={false} header={<span className="text-3xl font-mono" style={{ color: "white", display: "flex", justifyContent: "center", alignItems: "center" }}>DROP #000003</span>} key="3">
           <p>Coming Soon</p>
-        </StyledPanel>
+        </StyledPanel> */}
       </Collapse>
     </>
   )
