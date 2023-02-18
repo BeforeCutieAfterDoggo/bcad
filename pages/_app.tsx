b7380d0775fd5e9be2317529e7b7269be998a01a
Jonathan Miller <jon@jmill.dev>
2023-02-17 20:48:14 -0800

    Add fathom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,49 @@
-import 'tailwindcss/tailwind.css'
-import type { AppProps } from 'next/app'
-import Layout from '../components/layout'
-import type { ReactElement, ReactNode } from 'react'
-import type { NextPage } from 'next'
-import FrontPage from '../components/layout/frontpage'
+import "tailwindcss/tailwind.css";
+import type { AppProps } from "next/app";
+import Layout from "../components/layout";
+import type { ReactElement, ReactNode } from "react";
+import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import * as Fathom from "fathom-client";
 import "../style/App.css";
 
 type NextPageWithLayout = NextPage & {
-  getLayout?: (page: ReactElement) => ReactNode
-}
+  getLayout?: (page: ReactElement) => ReactNode;
+};
 
 type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout
-}
+  Component: NextPageWithLayout;
+};
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
-  const getLayout = Component.getLayout ?? ((page) => page)
-  return <Layout>
-        {/* {getLayout(<Component {...pageProps} />) }  */}
-        <Component {...pageProps} />
-      </Layout>  
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    Fathom.load("VBPBGBTF", {
+      includedDomains: ["bcad.one"],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on("routeChangeComplete", onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off("routeChangeComplete", onRouteChangeComplete);
+    };
+  }, []);
+
+  return (
+    <Layout>
+      {/* {getLayout(<Component {...pageProps} />) }  */}
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
-export default MyApp
+export default MyApp;
+function useEffect(arg0: () => () => void, arg1: never[]) {
+  throw new Error("Function not implemented.");
+}
