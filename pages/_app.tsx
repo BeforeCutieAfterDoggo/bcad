632bfbccae3635cb04734e1dbd8922d6179365c1
Jonathan Miller <jon@jmill.dev>
2023-02-17 20:55:37 -0800

    Fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,7 +20,7 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const router = useRouter();
 
   useEffect(() => {
-    Fathom.load(process.env.FATHOM_CODE, {
+    Fathom.load(process.env.NEXT_PUBLIC_FATHOM_CODE as string, {
       includedDomains: ["bcad.one"],
     });
 
