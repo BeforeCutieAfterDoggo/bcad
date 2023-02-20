97934db7cf1205235bd57eab423718b7cb840685
Jonathan Miller <jon@jmill.dev>
2023-02-20 11:07:01 -0800

    add plausible?

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import "tailwindcss/tailwind.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/layout";
-import { ReactElement, ReactNode, useEffect } from "react";
+import { PropsWithChildren, ReactElement, ReactNode, useEffect } from "react";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import * as Fathom from "fathom-client";
 import "../style/App.css";
 import { useFathom } from "../hooks/useFathom";
+import PlausibleProvider from "next-plausible";
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -23,11 +24,22 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
     spa: "auto",
   });
 
+  const PlausibleWrapper = ({ children }: PropsWithChildren<{}>) => {
+    if (process.env.NODE_ENV === "production") {
+      return (
+        <PlausibleProvider domain="bcad.one">{children}</PlausibleProvider>
+      );
+    }
+    return <>{children}</>;
+  };
+
   return (
-    <Layout>
-      {/* {getLayout(<Component {...pageProps} />) }  */}
-      <Component {...pageProps} />
-    </Layout>
+    <PlausibleWrapper>
+      <Layout>
+        {/* {getLayout(<Component {...pageProps} />) }  */}
+        <Component {...pageProps} />
+      </Layout>
+    </PlausibleWrapper>
   );
 }
 export default MyApp;
