33ba21224cf8df6a8b50c2f206af0b564133fd80
Lucy Qiu <lucy1049684100@gmail.com>
2023-06-10 12:56:43 -0700

    w

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,7 @@ import type { AppProps } from "next/app";
 import Layout from "../components/layout";
 import { PropsWithChildren, ReactElement, ReactNode, useEffect } from "react";
 import type { NextPage } from "next";
-import { useRouter } from "next/router";
 import "../style/App.css";
-import PlausibleProvider from "next-plausible";
-import CartProvider from "../pages/context/CartContext";
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -16,26 +13,10 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
-  const getLayout = Component.getLayout ?? ((page) => page);
-
-  const PlausibleWrapper = ({ children }: PropsWithChildren<{}>) => {
-    if (process.env.NODE_ENV === "production") {
-      return (
-        <PlausibleProvider domain="bcad.one">{children}</PlausibleProvider>
-      );
-    }
-    return <>{children}</>;
-  };
-
   return (
-    <CartProvider>
-      <PlausibleWrapper>
         <Layout>
-          {/* {getLayout(<Component {...pageProps} />) }  */}
           <Component {...pageProps} />
         </Layout>
-      </PlausibleWrapper>
-    </CartProvider>
   );
 }
 export default MyApp;
