348d02ced254a3f1bdf030f0d98efd6b6f3465b3
Jonathan Miller <jon@jmill.dev>
2024-10-14 15:15:23 +0700

    BCAD analytics

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,11 @@ export default function Home(): any {
     <>
       <Head>
         <title>BCAD</title>
+        <script
+          defer
+          src="https://umami-tawny-theta.vercel.app/script.js"
+          data-website-id="2f689c74-a3c1-41af-b14a-010a28c4f01f"
+        ></script>
       </Head>
       <main className="min-h-screen pb-14 flex-col justify-center relative">
         <Season2 />
